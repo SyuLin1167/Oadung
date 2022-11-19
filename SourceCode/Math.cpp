3e7506336f6c9dbe90c3842d943213c31ca31bca
jinsio <jinsio0815@icloud.com>
2022-11-19 16:23:07 +0900

    ベクトルの計算(完了&修正の可能性あり)

--- SourceCode/Math.cpp
+++ SourceCode/Math.cpp
@@ -1,1 +1,52 @@
 #include "Math.h"
+
+// @brief ベクトル同士の加算 //
+
+VECTOR operator+(const VECTOR& lhs, const VECTOR& rhs)
+{
+    VECTOR vRet{};
+    vRet.x = lhs.x + rhs.x;
+    vRet.y = lhs.y + rhs.y;
+    vRet.z = lhs.z + rhs.z;
+    return vRet;
+}
+
+// @brief ベクトル同士の減算 //
+
+VECTOR operator-(const VECTOR& lhs, const VECTOR& rhs)
+{
+    VECTOR vRet{};
+    vRet.x = lhs.x - rhs.x;
+    vRet.y = lhs.y - rhs.y;
+    vRet.z = lhs.z - rhs.z;
+    return vRet;
+}
+
+// @brief ベクトルのスカラー倍 //
+
+VECTOR operator*(const VECTOR& lhs, float s)
+{
+    VECTOR vRet{};
+    vRet.x = lhs.x * s;
+    vRet.y = lhs.y * s;
+    vRet.z = lhs.z * s;
+    return vRet;
+}
+
+// @brief ベクトルの演算子 //
+
+VECTOR operator+=(VECTOR& lhs, const VECTOR& rhs)
+{
+    lhs = lhs + rhs;
+    return lhs;
+}
+
+// @brief ベクトルのスカラー倍の演算子 //
+
+VECTOR operator*=(VECTOR& lhs, float rhs)
+{
+    lhs = lhs * rhs;
+    return lhs;
+}
+
+
--- SourceCode/Math.h
+++ SourceCode/Math.h
@@ -1,5 +1,42 @@
 #pragma once
-class Math
-{
-};
+#include<DxLib.h>
 
+/// <summary>
+/// ベクトル同士の加算
+/// </summary>
+/// <param name="lhs">左辺(VECTOR)</param>
+/// <param name="rhs">右辺(VECTOR)</param>
+/// <returns>2つのベクトルの和</returns>
+VECTOR operator+(const VECTOR& lhs, const VECTOR& rhs);
+
+/// <summary>
+/// ベクトル同士の減算
+/// </summary>
+/// <param name="lhs">左辺(VECOTR)</param>
+/// <param name="rhs">右辺(VECOTR)</param>
+/// <returns>2つのベクトルの差</returns>
+VECTOR operator-(const VECTOR& lhs, const VECTOR& rhs);
+
+/// <summary>
+/// ベクトルのスカラー倍
+/// </summary>
+/// <param name="lhs">左辺(VECTOR)</param>
+/// <param name="s">右辺(float)</param>
+/// <returns>ベクトルにfloatを掛けた値</returns>
+VECTOR operator*(const VECTOR& lhs, float s);
+
+/// <summary>
+/// ベクトルの演算子
+/// </summary>
+/// <param name="lhs">左辺(VECTOR)</param>
+/// <param name="rhs">右辺(VECTOR)</param>
+/// <returns>左辺に右辺を加算したベクトルの値</returns>
+VECTOR operator+=(VECTOR& lhs, const VECTOR& rhs);
+
+/// <summary>
+/// ベクトルのスカラー倍の演算子
+/// </summary>
+/// <param name="lhs">左辺(VECTOR)</param>
+/// <param name="rhs">右辺(float)</param>
+/// <returns>左辺に右辺を乗算したベクトルの値</returns>
+VECTOR operator*=(VECTOR& lhs, float rhs);
