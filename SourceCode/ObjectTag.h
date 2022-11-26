d630ee058ee779472d85ee74c13dae83b66de7e1
406-10 <>
2022-11-26 12:19:27 +0900

    タグ付け処理

--- SourceCode/ObjectTag.h
+++ SourceCode/ObjectTag.h
@@ -1,5 +1,19 @@
 #pragma once
-class ObjectTag
+#include<vector>
+
+/*種類別タグクラス*/
+enum class ObjectTag:unsigned char
 {
+    Player,     //プレイヤー
+    Archer,     //アーチャー
+    Enemy,      //エネミー
+};
+
+/*ループ制御用*/
+constexpr static ObjectTag ObjTagAll[] =
+{
+    ObjectTag::Player,
+    ObjectTag::Archer,
+    ObjectTag::Enemy,
 };
 
